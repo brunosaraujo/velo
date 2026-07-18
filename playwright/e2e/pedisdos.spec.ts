105875eeebd5a1c87a3cba90f1b930651ea62a43
Bruno Araujo <brunosisag@gmail.com>
2026-07-18 18:11:45 -0300

    Desafio sem os Test-ID

--- playwright/e2e/pedisdos.spec.ts
+++ playwright/e2e/pedisdos.spec.ts
@@ -17,12 +17,24 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   //await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-8AH4U5')
   //await page.getByLabel('Número do Pedido').fill('VLO-8AH4U5')
   await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-8AH4U5')
-  await page.getByTestId('search-order-button').click()
+  //await page.getByTestId('search-order-button').click()
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
   
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 })
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-8AH4U5')
+  // await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 })
+  // await expect(page.getByTestId('order-result-id')).toContainText('VLO-8AH4U5')
+  // await expect(page.getByText('VLO-8AH4U5')).toBeVisible({ timeout: 10_000 });
+  // await expect(page.getByTestId('order-result-VLO-8AH4U5')).toContainText('VLO-8AH4U5');
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('VLO-8AH4U5')).toBeVisible();
+  await expect(page.locator('#root')).toContainText('VLO-8AH4U5');  
+  
+
+  // await expect(page.getByTestId('order-result-status')).toBeVisible()
+  // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')  
+  // await expect(page.getByText('APROVADO')).toBeVisible();
+  // await expect(page.getByTestId('order-result-VLO-8AH4U5')).toContainText('APROVADO');
+  
+  await expect(page.getByText('APROVADO')).toBeVisible();
+  await expect(page.locator('#root')).toContainText('APROVADO');
 })
