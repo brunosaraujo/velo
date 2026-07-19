c1f793c1f3c974b9a593bf6f7c559d0dc205628d
Bruno Araujo <brunosisag@gmail.com>
2026-07-19 18:19:39 -0300

    Inclusão do helpers

--- playwright/e2e/pedisdos.spec.ts
+++ playwright/e2e/pedisdos.spec.ts
@@ -1,22 +1,7 @@
 import { test, expect } from '@playwright/test'
-
+import {gerarCodigoPedido} from '../suppport/helpers'
 /// AAA - Arrange, Act, Assert
 /// PAV - Preparar, Agir, Verificar 
-
-function gerarCodigoPedido() {
-  const prefixo = 'VLO';
-  
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let randomPart = '';
-
-  for (let i = 0; i < 6; i++){
-    const randomIndex = Math.floor(Math.random() * chars.length)
-    randomPart += chars[randomIndex]
-  }
-  
-  return `${prefixo}-${randomPart}`;
-}
-
  
 test('deve consultar um pedido aprovado', async ({ page }) => {
 
