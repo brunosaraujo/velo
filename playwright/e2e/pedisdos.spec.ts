d1075afdf2d165d38abf9709e0190e10f60c5921
Bruno Araujo <brunosisag@gmail.com>
2026-07-19 16:39:35 -0300

    Resolvendo o Desafio dos IDs

--- playwright/e2e/pedisdos.spec.ts
+++ playwright/e2e/pedisdos.spec.ts
@@ -4,8 +4,8 @@ import { test, expect } from '@playwright/test'
 /// PAV - Preparar, Agir, Verificar
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-// Arrange
-  await page.goto('http://localhost:5173/')  
+  // Arrange
+  await page.goto('http://localhost:5173/')
   // Checkpoint 1: Verificar se o texto "Velô Sprint" está presente na página
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
@@ -19,22 +19,33 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-8AH4U5')
   //await page.getByTestId('search-order-button').click()
   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
-  
+
   // Assert
   // await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 })
   // await expect(page.getByTestId('order-result-id')).toContainText('VLO-8AH4U5')
   // await expect(page.getByText('VLO-8AH4U5')).toBeVisible({ timeout: 10_000 });
   // await expect(page.getByTestId('order-result-VLO-8AH4U5')).toContainText('VLO-8AH4U5');
 
-  await expect(page.getByText('VLO-8AH4U5')).toBeVisible();
-  await expect(page.locator('#root')).toContainText('VLO-8AH4U5');  
-  
+  // xpath puro
+  const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-8AH4U5"]')
+
+  await expect(page.getByText('VLO-8AH4U5')).toBeVisible({ timeout: 10_000 })
+  await expect(orderCode).toBeVisible({ timeout: 10_000 })
+  await expect(page.locator('#root')).toContainText('VLO-8AH4U5')
+
+  const containerPedido = page.getByRole('paragraph')
+  // Expressão regular para pegar iniciando com pedido e terminando com pedido  
+  .filter({ hasText: /^Pedido$/ })
+    .locator('..')
+
+  await expect(containerPedido).toContainText('VLO-8AH4U5', { timeout: 10_000 })
+
 
   // await expect(page.getByTestId('order-result-status')).toBeVisible()
   // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')  
   // await expect(page.getByText('APROVADO')).toBeVisible();
   // await expect(page.getByTestId('order-result-VLO-8AH4U5')).toContainText('APROVADO');
-  
-  await expect(page.getByText('APROVADO')).toBeVisible();
-  await expect(page.locator('#root')).toContainText('APROVADO');
+
+  await expect(page.getByText('APROVADO')).toBeVisible({ timeout: 10_000 })
+  await expect(page.locator('#root')).toContainText('APROVADO')
 })
