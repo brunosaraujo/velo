6174ed16053ef5e9a2d980ed5a692ef399e23e3d
Bruno Araujo <brunosisag@gmail.com>
2026-07-19 21:38:08 -0300

    Validações de Interface Além do Tradicional com Snapshots

--- playwright/e2e/pedisdos.spec.ts
+++ playwright/e2e/pedisdos.spec.ts
@@ -51,27 +51,56 @@ test.describe('Consulta de Pedido', () => {
     // await expect(page.getByTestId('order-result-VLO-8AH4U5')).toContainText('VLO-8AH4U5')
 
     // xpath puro
-    const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-8AH4U5"]')
+    // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-8AH4U5"]')
 
-    await expect(page.getByText(order)).toBeVisible({ timeout: 10_000 })
-    await expect(orderCode).toBeVisible({ timeout: 10_000 })
-    await expect(page.locator('#root')).toContainText(order)
+    // await expect(page.getByText(order)).toBeVisible({ timeout: 10_000 })
+    // await expect(orderCode).toBeVisible({ timeout: 10_000 })
+    // await expect(page.locator('#root')).toContainText(order)
 
-    const containerPedido = page.getByRole('paragraph')
-      // Expressão regular para pegar iniciando com pedido e terminando com pedido  
-      .filter({ hasText: /^Pedido$/ })
-      .locator('..')
+    // const containerPedido = page.getByRole('paragraph')
+    //   // Expressão regular para pegar iniciando com pedido e terminando com pedido  
+    //   .filter({ hasText: /^Pedido$/ })
+    //   .locator('..')
 
-    await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+    // await expect(containerPedido).toContainText(order, { timeout: 10_000 })
 
 
-    // await expect(page.getByTestId('order-result-status')).toBeVisible()
-    // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')  
-    // await expect(page.getByText('APROVADO')).toBeVisible();
-    // await expect(page.getByTestId('order-result-VLO-8AH4U5')).toContainText('APROVADO');
+    // // await expect(page.getByTestId('order-result-status')).toBeVisible()
+    // // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')  
+    // // await expect(page.getByText('APROVADO')).toBeVisible();
+    // // await expect(page.getByTestId('order-result-VLO-8AH4U5')).toContainText('APROVADO');
 
-    await expect(page.getByText('APROVADO')).toBeVisible({ timeout: 10_000 })
-    await expect(page.locator('#root')).toContainText('APROVADO')
+    // await expect(page.getByText('APROVADO')).toBeVisible({ timeout: 10_000 })
+    // await expect(page.locator('#root')).toContainText('APROVADO')
+
+    await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order}
+      - img
+      - text: APROVADO
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: Midnight Black
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: aero Wheels
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: Bruno Araujo
+      - paragraph: Email
+      - paragraph: brunosisag@gmail.com
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: À Vista
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `)
   })
 
   test('deve exibir mensagem quando o pedido não é encontrato', async ({ page }) => {
